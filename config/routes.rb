5e5fc0dc789072638ae857adfaeb161838e43f1c
VikiAnn <victoria.harrod@gmail.com>
2023-03-30 11:25:49 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
+    mount VandalUi::Engine, at: '/vandal'
+    # your routes go here
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
