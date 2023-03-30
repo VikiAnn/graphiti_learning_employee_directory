d1f0fac61a6f92bb6e56e60873a74799639abaa7
VikiAnn <victoria.harrod@gmail.com>
2023-03-30 11:28:58 -0600

    Generate employee model/table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Employee.delete_all # Ensure the DB is cleaned each run
+
+100.times do
+  Employee.create! first_name: Faker::Name.first_name,
+                   last_name: Faker::Name.last_name,
+                   age: rand(20..80)
+end
